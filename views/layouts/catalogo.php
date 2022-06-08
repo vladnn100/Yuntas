f254ff95c1853f002691ea63547806ead5533f5c
AldairNM <navarromachado76@gmail.com>
2022-06-08 16:37:07 -0500

    catalogo

---
+++ views/layouts/catalogo.php
@@ -0,0 +1,16 @@
+<div class="catalogo">
+    <div class="catalogoT">
+        <h1>CONOCE MÁS DE NUESTROS PRODUCTOS</h1>
+    </div>
+    <div class="catalogoB">
+        <a class="enlaceC" href="#">VER CATÁLOGO</a>
+        <a class="enlaceC" href="#">COTIZAR</a>
+    </div>
+</div>
+        
+
+
+
+
+
+
--- views/main/index.php
+++ views/main/index.php
@@ -10,7 +10,7 @@
     
     <meta name="keywords" content="Yuntas Producciones, luces led,letrero led, yuntas,pantallas led,calidad de imagen,efectos 3d led,Neon Led,luces navideñas,iluminación led, yuntas producciones,barras luces led, ventilador holografico, holograma 3d, publicidad,marketing, luces led de colores,luces led navidad">
 
-    <link rel="stylesheet" href="../public/css/home.css" >
+    <link rel="stylesheet" href="public/css/home.css" >
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>YUNTAS</title>
